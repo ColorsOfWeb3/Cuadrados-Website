8b97cd68d410f4e8993a5a212758c70f60c380fc
Nopirat <almoratg@gmail.com>
2023-08-16 00:56:20 +0200

    style improvement

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ import { mint } from "./contract.js";
 const socket = io();
 let data = {};
 
-const supply = 999;
-const colorDefault = "#D6D1D8";
+const supply = 1225;
+const dafaultColor = "95A5A6";
 
 function initSVG(totalItems) {
     const gridDiv = document.querySelector(".grid");
@@ -15,13 +15,16 @@ function initSVG(totalItems) {
 }
 
 function createSVG(id) {
+    const data = getElementData(id);
+
     const svgNS = "http://www.w3.org/2000/svg";
     const viewBox = "0 0 100 100";
     const rectWidth = "100%";
     const rectHeight = "100%";
-    let fill = getColor(id);
+    let color = data ? data.color : dafaultColor;
 
-
+    const container = document.createElement("div");
+    container.classList = "svg-container"
 
     const svg = document.createElementNS(svgNS, "svg");
     svg.setAttribute("xmlns", svgNS);
@@ -31,16 +34,23 @@ function createSVG(id) {
     const rect = document.createElementNS(svgNS, "rect");
     rect.setAttribute("width", rectWidth);
     rect.setAttribute("height", rectHeight);
-    rect.setAttribute("fill", fill);
+    rect.setAttribute("fill", `#${color}`);
 
     svg.appendChild(rect);
+
+    const tooltip = document.createElement("div");
+    tooltip.classList = "svg-tooltip";
+    tooltip.textContent = color;
+
+    container.appendChild(svg);
+    container.appendChild(tooltip);
 
     svg.addEventListener("click", async function () {
         const id = this.getAttribute("id");
         mint(id);
     });
 
-    return svg;
+    return container;
 }
 
 function updateSVG(id, color) {
@@ -63,11 +73,9 @@ async function getData() {
 
 }
 
-function getColor(id) {
-    let color = colorDefault
+function getElementData(id) {
     const result = data.find(data => data.tokenId == id);
-    if (result != undefined) color = `#${result.color}`
-    return color;
+    return result;
 }
 
 socket.on('update', data => {
@@ -81,16 +89,33 @@ document.addEventListener("DOMContentLoaded", async function () {
 });
 
 function updateElementSize() {
-    const vw = window.innerWidth - 40;
-    const vh = window.innerHeight - 40;
-    const nw = Math.sqrt((vw * supply) / vh)
-    const gap = (vw / nw) / 6
-    const nh = Math.sqrt((vh * supply) / (vw - (nw * (gap * 2))))
-    const result = vh / nh;
+    const gapRatio = 0.2;
+    const padding = 20;
 
-    const element = document.getElementById('grid'); // Replace with your element's ID
-    element.style.gridTemplateColumns = `repeat(auto-fill, ${result}px)`; // Adjust the property name as needed
-    element.style.gridTemplateRows = `repeat(auto-fill, ${result}px)`; // Adjust the property name as needed
+    const windowWidth = window.innerWidth;
+    const windowHeight = window.innerHeight;
+
+    const vw = (windowWidth - padding) * (1 - gapRatio);
+    const vh = (windowHeight - padding) * (1 - gapRatio);
+
+    
+    let area = vw * vh;
+    let squareArea = area / supply;
+    let width = Math.sqrt(squareArea);
+
+    let nw = vw / width;
+    let nh = vh / width;
+
+    const gapW = (windowWidth * gapRatio) / nw;
+    const gapH = (windowHeight * gapRatio) / nh;
+
+    const gap = Math.min(gapW, gapH)
+
+    const result = width - ((width + gap) / Math.max(nw, nh));
+
+    const element = document.getElementById('grid');
+    element.style.gridTemplateColumns = `repeat(auto-fit, ${result}px)`;
+    element.style.gridAutoRows = `${result}px`;
     element.style.gap = `${gap}px`
 }
 
